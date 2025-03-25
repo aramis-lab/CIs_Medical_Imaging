4ccdea8051a4a8d2f3e9f3b6e44e66cb7f71cc3f
iFyroxx <charles.heitz1308@gmail.com>
2025-03-25 11:38:07 +0100

    Removed data from repo

--- src/summary_stats/__init__.py
+++ src/summary_stats/__init__.py
@@ -11,12 +11,22 @@ def median(x, threshold, axis=None):
 def trimmed_mean(x, threshold, axis=None):
     return trim_mean(x, threshold, axis=axis)
 
+def variance(x, threshold, axis=None):
+    return np.var(x, axis=axis)
+
+def IQR(x, threshold, axis=None):
+    q3 = np.percentile(x, 75, axis=axis)
+    q1 = np.percentile(x, 25, axis=axis)
+    return q3 - q1
+
 def get_statistic(statistic):
 
     statistic_dict = {
         "mean": mean,
         "median": median,
-        "trimmed_mean": trimmed_mean
+        "trimmed_mean": trimmed_mean,
+        "variance": variance,
+        "IQR": IQR
     }
 
     return statistic_dict.get(statistic, None)
