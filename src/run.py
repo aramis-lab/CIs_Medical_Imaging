24411a20a5b5add44b6beb5fd60d3cb3af77c0fe
iFyroxx <charles.heitz1308@gmail.com>
2025-04-08 11:35:41 +0200

    Parallelized across all benchmarking instances as well

--- src/run.py
+++ src/run.py
@@ -18,7 +18,22 @@ def extract_df(df, metric, task):
     df = df[(df["score"] == metric) & (df["subtask"] == task)]
     return df.drop(["score", "subtask", "task", "case", "alg_number", "phase"], axis=1)
 
-def make_kdes_and_compute_metrics(df, task, config):
+def get_benchmark_instances(cfg):
+    benchmark_instances = []
+    df = pd.read_csv(os.path.join(BASE_DIR, cfg.relative_data_path))
+    for task in cfg.tasks:
+        df_task = extract_df(df, cfg.metric, task)
+        algos = df_task["alg_name"].unique()
+        for algo in algos:
+            benchmark_instances.append((task, algo))
+    # Sort by task
+    benchmark_instances.sort(key=lambda x: (x[0], x[1]))
+    # Remove duplicates
+    benchmark_instances = list(dict.fromkeys(benchmark_instances))
+
+    return np.array(benchmark_instances)
+
+def make_kdes_and_compute_metrics(df, task, algo, config):
 
     # Retrieve configuration and set up variables
     ci_methods = config.ci_methods
@@ -29,66 +44,66 @@ def make_kdes_and_compute_metrics(df, task, config):
 
     kernel = get_kernel(config.kernel)
 
-    # Iterate over algorithms
-    for algo in tqdm(df["alg_name"].unique()):
-        DSCs = df[df["alg_name"] == algo]["value"].to_numpy()
+    DSCs = df[df["alg_name"] == algo]["value"].to_numpy()
 
-        values_span = np.max(DSCs) - np.min(DSCs)
-        # Define the grid for KDE
-        if np.isinf(a):
-            min_val = np.min(DSCs) - 0.1 * values_span
-        else:
-            min_val = a
-        
-        if np.isinf(b):
-            max_val = np.max(DSCs) + 0.1 * values_span
-        else:
-            max_val = b
-        x = np.linspace(min_val, max_val, 10000)  # You can change the resolution of x
-        alphas = np.ones(len(DSCs))
+    values_span = np.max(DSCs) - np.min(DSCs)
+    # Define the grid for KDE
+    if np.isinf(a):
+        min_val = np.min(DSCs) - 0.1 * values_span
+    else:
+        min_val = a
+    
+    if np.isinf(b):
+        max_val = np.max(DSCs) + 0.1 * values_span
+    else:
+        max_val = b
+    x = np.linspace(min_val, max_val, 10000)  # You can change the resolution of x
+    alphas = np.ones(len(DSCs))
 
-        dist_to_bounds = np.min([DSCs-a, b-DSCs], axis=0)
+    dist_to_bounds = np.min([DSCs-a, b-DSCs], axis=0)
 
-        # Iterative weighted KDE estimation
-        for _ in range(1):
-            y = weighted_kde(DSCs, x, dist_to_bounds, kernel, alphas)
-            indices = np.searchsorted(x, DSCs)
-            initial_estimates = y[indices]
-            log_g = np.mean(np.log(initial_estimates))
-            g = np.exp(log_g)
-            alphas = (initial_estimates / g) ** (-1/2) * (1-np.exp(-1e6*dist_to_bounds))
-        
+    # Iterative weighted KDE estimation
+    for _ in range(1):
         y = weighted_kde(DSCs, x, dist_to_bounds, kernel, alphas)
-        samples = sample_weighted_kde(y, x, 1000000)
+        indices = np.searchsorted(x, DSCs)
+        initial_estimates = y[indices]
+        log_g = np.mean(np.log(initial_estimates))
+        g = np.exp(log_g)
+        alphas = (initial_estimates / g) ** (-1/2) * (1-np.exp(-1e6*dist_to_bounds))
+    
+    y = weighted_kde(DSCs, x, dist_to_bounds, kernel, alphas)
+    samples = sample_weighted_kde(y, x, 1000000)
 
-        # Compute true statistic
-        true_value = statistic(samples)
+    # Compute true statistic
+    true_value = statistic(samples)
 
-        for n in tqdm(config.sample_sizes):
-            new_row = {"subtask": task, "alg_name": algo, "n": n}
-            samples = sample_weighted_kde(y,x, config.n_samples*n).reshape(config.n_samples, n)
+    for n in tqdm(config.sample_sizes):
+        new_row = {"subtask": task, "alg_name": algo, "n": n}
+        samples = sample_weighted_kde(y,x, config.n_samples*n).reshape(config.n_samples, n)
 
-            for method in ci_methods:
-                CIs, nan_proportion = compute_CIs(samples, method, statistic)
-                coverage, proportion_oob, width = compute_metrics(CIs, true_value)
-                new_row.update({f"{method}_coverage": coverage, f"{method}_proportion_oob": proportion_oob, f"{method}_width": width, f"{method}_nans": nan_proportion})
-            results = pd.concat([results, pd.DataFrame(new_row, index=[0])], ignore_index=True)
+        for method in ci_methods:
+            CIs, nan_proportion = compute_CIs(samples, method, statistic)
+            coverage, proportion_oob, width = compute_metrics(CIs, true_value)
+            new_row.update({f"{method}_coverage": coverage, f"{method}_proportion_oob": proportion_oob, f"{method}_width": width, f"{method}_nans": nan_proportion})
+        results = pd.concat([results, pd.DataFrame(new_row, index=[0])], ignore_index=True)
     return results
 
-def process_subtask(task, cfg):
-    print(f"Running KDE for metric {cfg.metric} and subtask {task}")
+def process_subtask(task, algo, cfg):
+    print(f"Running KDE for metric {cfg.metric}, subtask {task} and algorithm {algo}")
     df = pd.read_csv(os.path.join(BASE_DIR, cfg.relative_data_path))
     df = extract_df(df, cfg.metric, task)
-    results = make_kdes_and_compute_metrics(df, task, cfg)
+    results = make_kdes_and_compute_metrics(df, task, algo, cfg)
     return results
 
 @hydra.main(config_path="cfg", config_name="config", version_base="1.3.2")
 def main(cfg: DictConfig):
     aggreggated_results = pd.DataFrame()
 
+    benchmark_instances = get_benchmark_instances(cfg)
+
     # Use joblib to parallelize tasks
-    with joblib.Parallel(n_jobs=-1) as parallel:
-        results = parallel(joblib.delayed(process_subtask)(task, cfg) for task in cfg.tasks[-1:])
+    with joblib.Parallel(n_jobs=4) as parallel:
+        results = parallel(joblib.delayed(process_subtask)(task, algo, cfg) for task, algo in benchmark_instances[-1:])
 
     # Combine results from all tasks
     for result in results:
