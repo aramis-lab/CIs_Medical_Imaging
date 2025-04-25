2173155f0b1d4237724cc70fb413c53888f00dd8
Charles HEITZ <charles.heitz@icm-institute.org>
2025-04-25 16:16:52 +0200

    Added config and modified run.sh accordingly

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=hydra_sweep
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --time=02:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --partition=your_partition_name
+
+module load python
+conda activate CI
+
+python main.py -m \
+  --config-path cfg \
+  --config-name config.yaml \
+  metric=dsc,nsd \
+  summary_stat=mean,median,trimmed_mean,std,iqr_length
