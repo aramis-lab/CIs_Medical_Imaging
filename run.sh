b992e84c776a0f62e6001c8604d1eb8890a4f9b8
Charles HEITZ <charles.heitz@icm-institute.org>
2025-05-15 14:19:36 +0200

    Changed sweep in run.sh

--- run.sh
+++ run.sh
@@ -13,18 +13,21 @@
 module load python
 conda activate CI
 
+METRICS=(dsc nsd)
+
+# Extract metrics into comma-separated string
+metrics_csv=$(IFS=','; echo "${METRICS[*]}")
+
 python src/utils/extract_df_and_make_instance_list.py -m \
-    metric="${METRICS[@]}" \
+    metric="$metrics_csv" \
     kernel=epanechnikov \
     summary_stat=mean,median,trimmed_mean,std,iqr_length
 
-METRICS=(dsc nsd)
 for METRIC in "${METRICS[@]}"; do
   LIST_FILE="instances_list/${METRIC}.txt"
   mapfile -t TASKS_AND_ALGOS < "$LIST_FILE"
   PAIR="${TASKS_AND_ALGOS[$SLURM_ARRAY_TASK_ID]}"
-  TASK=$(echo "$PAIR" | cut -d ' ' -f1)
-  ALGO=$(echo "$PAIR" | cut -d ' ' -f2)
+  read -r TASK ALGO <<< "$PAIR"
 
   python src/run.py -m \
     metric="$METRIC" \
