6eba0e3d5926a609898600e8f0b556b8e4f47950
Charles HEITZ <charles.heitz@icm-institute.org>
2025-05-15 13:27:58 +0200

    Changed path in extract_df_and_make_instance_list

--- src/utils/extract_df_and_make_instance_list.py
+++ src/utils/extract_df_and_make_instance_list.py
@@ -24,8 +24,8 @@ def get_benchmark_instances(BASE_DIR, cfg):
 
 if __name__ == "__main__":
     BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
-    cfg_path = "../cfg/config.yaml"
-    cfg = OmegaConf.load(cfg_path)
+    cfg_path = "cfg/config.yaml"
+    cfg = OmegaConf.load(os.path.join(BASE_DIR, cfg_path))
     instances = get_benchmark_instances(BASE_DIR, cfg)
 
     with open("../benchmark_list.txt", "w") as f:
