fd6eae354afc117cbe5fdd24bace577b262c273b
Charles HEITZ <charles.heitz@icm-institute.org>
2025-05-15 13:20:51 +0200

    Changed imports in make_instance_list

--- src/utils/make_instance_list.py
+++ src/utils/make_instance_list.py
@@ -1,12 +1,14 @@
-# make_benchmark_list.py
 from extract_df_and_instances import get_benchmark_instances
-from CIs_Medical_Imaging.src.run import BASE_DIR
 from omegaconf import OmegaConf
+import os
 
-cfg_path = "cfg/config.yaml"
-cfg = OmegaConf.load(cfg_path)
-instances = get_benchmark_instances(BASE_DIR, cfg)
 
-with open("../benchmark_list.txt", "w") as f:
-    for task, algo in instances:
-        f.write(f"{task} {algo}\n")
+if __name__ == "__main__":
+    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+    cfg_path = "cfg/config.yaml"
+    cfg = OmegaConf.load(cfg_path)
+    instances = get_benchmark_instances(BASE_DIR, cfg)
+
+    with open("../benchmark_list.txt", "w") as f:
+        for task, algo in instances:
+            f.write(f"{task} {algo}\n")
