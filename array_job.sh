2a1bdac641473978736b40e1dd4817b57dc9850a
Charles HEITZ <charles.heitz@icm-institute.org>
2025-05-19 08:37:03 +0200

    Changed trimmed_mean

--- array_job.sh
+++ array_job.sh
@@ -3,9 +3,8 @@
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --time=20:00:00
 #SBATCH --nodes=1
-#SBATCH -A qhn@cpu
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --qos=qos_cpu-t3
 #SBATCH --partition=cpu_p1
 #SBATCH --hint=nomultithread
