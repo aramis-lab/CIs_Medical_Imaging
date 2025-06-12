8d1a1ab0f1f49aeb35e51c408d8b6fb11dd54ca0
Charles HEITZ <charles.heitz@icm-institute.org>
2025-06-12 09:46:06 +0200

    Removed parametric t and z for trimmed mean, as assumptions don't hold

--- src/intervals_and_metrics/authorized_methods.py
+++ src/intervals_and_metrics/authorized_methods.py
@@ -1,13 +1,13 @@
 def get_authorized_methods(summary_stat, metric):
     if metric in ["nsd", "boundary_iou", "cldice", "dsc", "iou"]:
-        if summary_stat in ["mean", "trimmed_mean"]:
+        if summary_stat in ["mean"]:
             return {"param_t", "param_z", "percentile", "basic", "bca"}
-        elif summary_stat in ["median", "iqr_length", "std"]:
+        elif summary_stat in ["median", "iqr_length", "std", "trimmed_mean"]:
             return {"percentile", "basic", "bca"}
     elif metric in ["assd", "hd", "hd_95", "masd"]:
-        if summary_stat in ["mean", "trimmed_mean"]:
+        if summary_stat in ["mean"]:
             return {"param_t", "param_z", "percentile", "basic", "bca"}
-        elif summary_stat in ["median", "iqr_length", "std"]:
+        elif summary_stat in ["median", "iqr_length", "std", "trimmed_mean"]:
             return {"percentile", "basic", "bca"}
     elif metric in ["accuracy", "npv", "ppv", "precision", "recall", "sensitivity", "specificity", "balanced_accuracy", "f1_score", "fbeta_score", "mcc"]:
         return {"percentile", "basic", "bca", "agresti_coull", "wilson", "wald", "param_z", "cloper_pearson", "exact"}
