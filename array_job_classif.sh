8ed9cae1aec4f34c4a582cff63756764e53ce783
Charles HEITZ <charles.heitz@icm-institute.org>
2025-07-22 09:08:51 +0200

    Changed config to finish computing macro metrics

--- array_job_classif.sh
+++ array_job_classif.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH -A zcd@cpu
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=2
 #SBATCH --qos=qos_cpu-t3
 #SBATCH --partition=cpu_p1
 #SBATCH --hint=nomultithread
@@ -16,7 +16,7 @@ conda activate CI
 
 # Load all task-algo pairs
 # METRICS=(accuracy f1_score balanced_accuracy mcc auc ap)
-METRICS=(mcc)
+METRICS=(auc ap f1_score)
 AVERAGE=macro
 ALL_PAIRS=()
 
--- run_classif.sh
+++ run_classif.sh
@@ -4,13 +4,12 @@ module load python
 conda activate CI
 
 # METRICS=(accuracy f1_score balanced_accuracy mcc auc ap)
-METRICS=(mcc)
+METRICS=(auc ap f1_score)
 metrics_csv=$(IFS=','; echo "${METRICS[*]}")
 
 # Preprocess instance lists
 python src/utils/extract_df_and_make_instance_list.py --config-name=config_classif -m\
   metric="$metrics_csv" \
-  +task=chexpert_pleural_effusion \
   kernel=epanechnikov
 
 # Count total task-algo pairs
