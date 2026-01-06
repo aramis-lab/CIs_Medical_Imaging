63c7aa98463e97499f921301442af0b98edd8f12
Pascaline Andre <pascaline.a0307@gmail.com>
2026-01-06 09:54:50 +0100

    Final changes paper plot

--- src/paper_plots/main/fig6_hoeffding_vs_param_t.py
+++ src/paper_plots/main/fig6_hoeffding_vs_param_t.py
@@ -12,6 +12,7 @@ def plot_fig6_hoeffding_vs_param_t(root_folder:str, output_path:str):
     n_values = np.linspace(10, 1000, 200)
 
     hoeffding_width = 2 * np.sqrt(np.log(2 / 0.05) / (2 * n_values))
+
     typical_param_t_width = 2 * t.ppf(0.975, n_values - 1) * 0.220090 / np.sqrt(n_values)
     empirical_bernstein_width = 2 * (0.220090*np.sqrt(2  * np.log(4 / 0.05) / n_values)+ 7 * np.log(4 / 0.05) / (3 * (n_values - 1)))
 
@@ -31,11 +32,7 @@ def plot_fig6_hoeffding_vs_param_t(root_folder:str, output_path:str):
     inferred_width_Q1 = 2 * t.ppf(0.975, n_values - 1) * np.sqrt(var_Q1.median()) / np.sqrt(n_values)
     inferred_width_Q3 = 2 * t.ppf(0.975, n_values - 1) * np.sqrt(var_Q3.median()) / np.sqrt(n_values)
     plt.figure(figsize=(8, 6))
-    # plt.plot(n_values, worst_param_t_width, label="Worst-case Parametric t Width", color='#DE8F05')
-    plt.plot(n_values, hoeffding_width, label="Hoeffding", color='#0173B2', linestyle='-')
-    plt.plot(n_values, empirical_bernstein_width, label=fr"Empirical Bernstein (median $\hat\sigma=0.2200$)", color='#DE8F05', linestyle='-')
-    plt.plot(n_values, typical_param_t_width, label=fr"Parametric t (median $\hat\sigma=0.2200$)", color='#CC78BC')
-
+   # plt.plot(n_values, worst_param_t_width, label="Worst-case Parametric t Width", color='#DE8F05')
     plt.plot(
     n_values,
     inferred_width_Q1,
@@ -54,8 +51,20 @@ def plot_fig6_hoeffding_vs_param_t(root_folder:str, output_path:str):
         alpha=0.7,
         label=fr"Parametric t Q3 ($\hat\sigma={np.sqrt(var_Q3.median()):.4f}$)"
     )
-    plt.scatter(df_param_t_median.index, df_param_t_median.values, label="Parametric t (observed)", color="#3C220C", marker='x', zorder=5)
-    plt.scatter(df_percentile_median.index, df_percentile_median.values, label="Percentile bootstrap (observed)", color='#029E73', marker='D',zorder=5)
+    # plt.fill_between(
+    # df_param_t_Q1.index,
+    # df_param_t_Q1.values,
+    # df_param_t_Q3.values,
+    # alpha=0.6,
+    # color="#CC78BC",
+    # label="Parametric t IQR")
+
+    
+    plt.plot(n_values, hoeffding_width, label="Hoeffding", color='#0173B2', linestyle='--')
+    plt.plot(n_values, empirical_bernstein_width, label="Empirical Bernstein", color='#DE8F05', linestyle='-.')
+    plt.plot(n_values, typical_param_t_width, label="Median Parametric t", color='#CC78BC')
+    plt.scatter(df_percentile_median.index, df_percentile_median.values, label="Percentile, CIs of mean of DSC", color='#029E73', marker='D',zorder=5)
+    plt.scatter(df_param_t_median.index, df_param_t_median.values, label="Parametric t, CIs of mean of DSC", color="#3C220C", marker='x', zorder=5)
     plt.xlabel("Sample Size (n)")
     plt.ylabel("Width of Confidence Interval")
     plt.title("Comparison of concentration inequalities, parametric t and percentile bootstrap")
